c887a5faa36bcdec8cd3841518781673bb8d5956
azat valiev <azvaliev@gmail.com>
2022-08-12 10:06:22 -0500

    Moved copilot to seperate file

---
+++ lua/copilot.lua
@@ -0,0 +1,17 @@
+-----------------------------------------------------------
+-- Github Copilot
+-----------------------------------------------------------
+g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap('i', '<C-J>', 'copilot#Accept(\'<CR>\')', { silent = true, expr = true })
+g.copilot_filetypes = {
+	['*'] = false,
+	['javascript'] = true,
+	['typescript'] = true,
+	['lua'] = false,
+	['rust'] = true,
+	['c'] = true,
+	['c#'] = true,
+	['c++'] = true,
+	['go'] = true,
+	['python'] = true,
+	}
